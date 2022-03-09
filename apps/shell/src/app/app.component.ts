0fe573fd2046243520cf5d8e3c82187fa4464562
José Saldaña Perez <j.saldanaperez@gmail.com>
2022-03-09 11:18:54 +0100

    added repro

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'multi-language-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
